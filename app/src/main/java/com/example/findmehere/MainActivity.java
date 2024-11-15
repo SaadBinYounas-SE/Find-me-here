fa067766beb62db87ab8ae805c1d663e5e5be08e
Ajwa Asif <ajwaasif73@gmail.com>
2024-11-15 21:30:56 +0500

    activity_main set...login errors corrected

--- app/src/main/java/com/example/findmehere/MainActivity.java
+++ app/src/main/java/com/example/findmehere/MainActivity.java
@@ -4,21 +4,42 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tlNav;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        init();
     }
+
+    protected void init(){
+        tlNav=findViewById(R.id.tlNav);
+        viewPager=findViewById(R.id.viewPager);
+
+        tlNav.setupWithViewPager(viewPager);
+        MyAdaptorViewPage vpAdaptor=new MyAdaptorViewPage(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+//
+//        vpAdaptor.addFragment(new homeFragment(),"Home");
+//        vpAdaptor.addFragment(new newPostFragment(),"Post");
+//        vpAdaptor.addFragment(new MessegesContact(),"Message");
+//        vpAdaptor.addFragment(new Profile1_fragment(),"Profile");
+
+        viewPager.setAdapter(vpAdaptor);
+
+        tlNav.setTabTextColors(ContextCompat.getColor(this, R.color.unselected_tab_color), ContextCompat.getColor(this, R.color.selected_tab_color));
+    }
+
 }
---
+++ app/src/main/java/com/example/findmehere/loginActivity.java
@@ -0,0 +1,17 @@
+package com.example.findmehere;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
